bcfbff6e35672de3f8cd7ccb70910a88ac7a84e2
Alejandro Guirado Garcia <alejandro.guirado10@gmail.com>
2015-02-13 17:49:37 -0700

    Lenguajes de programación

---
+++ Producto2/juego.c
@@ -0,0 +1,19 @@
+
+/* Hello World in C, Ansi-style */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(void)
+{
+puts("Hola! Trataré de adivinar un número."
+"Piensa un número entre 1 y 10.Hello World!");
+sleep(5);
+puts("Ahora multiplícalo por 9.");
+sleep(5);
+puts("Si el número tiene 2 dígitos, súmalos entre si: Ej. 36 -> 3+6=9. Si tu número tiene un solo dígito, súmale 0.");
+sleep(5);
+puts("Al número resultante súmale 4.");
+sleep(10);
+puts("Muy bien. El resultado es 13 :3");
+return(EXIT_SUCCESS);
+}
---
+++ Producto2/juego.py
@@ -0,0 +1,15 @@
+# Hello World in Python
+import time
+print "Hola! Tratare de adivinar un numero. Piensa un numero entre 1 y 10."
+import time
+time.sleep(5)
+print "Ahora multiplicalo por 9."
+import time
+time.sleep(5)
+print "Si el numero tiene 2 digitos, sumalos entre si: Ej. 36 -> 3+6=9. Si tu numero tiene un solo digito, sumale 0."
+import time
+time.sleep(5)
+print "Al numero resultante sumale 4."
+import time
+time.sleep(10)
+print "Muy bien. El resultado es 13 :3 "
